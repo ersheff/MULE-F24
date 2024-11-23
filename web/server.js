5de7732ed89629eaa1cd6a77ec2e9d9d6fafaf1f
Eric Sheffield <ersheff@gmail.com>
2024-11-23 18:40:39 -0500

    changed path again

--- web/server.js
+++ web/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import { createServer } from "node:http";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = createServer(app);
 
-app.use(express.static("./"));
+// Resolve the path to the parent folder (MULE-F24)
+const parentFolderPath = path.resolve(__dirname, "..");
+app.use(express.static(parentFolderPath));
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
